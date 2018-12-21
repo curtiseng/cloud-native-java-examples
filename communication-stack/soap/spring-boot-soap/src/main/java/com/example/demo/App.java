e7c69aada8d919f978471a1ffdfd2d32e1026246
zifeng <zifeng.1024@gmail.com>
2018-12-21 16:55:13 +0800

    init spring boot soap

--- communication-stack/soap/spring-boot-soap/src/main/java/com/example/demo/App.java
+++ communication-stack/soap/spring-boot-soap/src/main/java/com/example/demo/App.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yangzifeng
+ */
 @SpringBootApplication
-public class DemoApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
