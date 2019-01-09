5d50760ec183aca8a7b253af9e54fd7d9260e226
zifeng <zifeng.1024@gmail.com>
2019-01-09 10:42:00 +0800

    spring boot web service refactor.

--- communication-stack/soap/spring-boot-soap/server/src/main/java/com/example/demo/ws/GetCountryResponse.java
+++ communication-stack/soap/spring-boot-soap/server/src/main/java/com/example/demo/ws/GetCountryResponse.java
@@ -8,11 +8,7 @@
 
 package com.example.demo.ws;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 
 
 /**
