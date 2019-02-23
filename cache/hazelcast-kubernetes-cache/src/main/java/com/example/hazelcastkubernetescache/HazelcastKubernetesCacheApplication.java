80ff8c9b341522e339b7018c0e47bd2af0764ba7
zifeng <zifeng.1024@gmail.com>
2019-02-23 17:20:32 +0800

    hazelcast with kubernetes demo initial.

---
+++ cache/hazelcast-kubernetes-cache/src/main/java/com/example/hazelcastkubernetescache/HazelcastKubernetesCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.hazelcastkubernetescache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastKubernetesCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazelcastKubernetesCacheApplication.class, args);
+	}
+
+}
